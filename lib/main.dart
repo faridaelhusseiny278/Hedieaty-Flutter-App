9f5cca9c16234be2a5d1d0bb41c766e2625d90a9
faridaelhusseiny278 <faridaelhussieny@gmail.com>
2024-11-24 18:28:34 +0200

    add tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,117 @@
 import 'package:flutter/material.dart';
-// Import all the pages here
+import 'package:motion_tab_bar/MotionTabBarController.dart';
+import 'package:motion_tab_bar/MotionTabBar.dart';
+import 'package:motion_tab_bar/MotionBadgeWidget.dart';
 import 'home_page.dart';
 import 'event_list_page.dart';
-import 'gift_list_page.dart';
-import 'gift_details_page.dart';
-import 'my_pledged_gifts_page.dart';
 import 'profile_page.dart';
+import 'my_pledged_gifts_page.dart';
 
-void main() => runApp(MaterialApp(
-  // Set the initial page
-  initialRoute: '/home', // Default page when the app starts
-  theme: ThemeData(
-    fontFamily: 'Nunito',
-    colorScheme: ColorScheme.fromSwatch().copyWith(
-      primary: Color(0xFF3A3A3A),
-      secondary: Color(0xFFB3E5FC),
-    ),
-  ),
-  routes: {
-    // Define routes for each page
-    '/home': (context) => HomePage(),
-    '/gifts': (context) => GiftListPage(),
-    '/events': (context) => EventListPage(),
-    '/pledged_gifts': (context) => PledgedListPage(),
-    '/profile': (context) => ProfilePage(),
-  },
-));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hedieaty',
+      theme: ThemeData(
+        fontFamily: 'Nunito',
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF3A3A3A),
+          secondary: const Color(0xFFB3E5FC),
+        ),
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
+  late MotionTabBarController _motionTabBarController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the MotionTabBarController
+    _motionTabBarController = MotionTabBarController(
+      initialIndex: 0, // Set initial index to 0 for the first tab
+      length: 4,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _motionTabBarController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        controller: _motionTabBarController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          EventListPage(),
+          HomePage(),
+          PledgedListPage(),
+          ProfilePage(),
+        ],
+      ),
+      bottomNavigationBar: MotionTabBar(
+        controller: _motionTabBarController,
+        initialSelectedTab: "Home",
+        labels: const ["Events", "Home","Pledged Gifts", "Profile"],
+        icons: const [
+          Icons.calendar_month,
+          Icons.home,
+          Icons.card_giftcard,
+          Icons.person,
+        ],
+        badges: [
+          const MotionBadgeWidget(
+            text: '10+',
+            color: Colors.red,
+          ), // Badge for "Events"
+          const MotionBadgeWidget(
+            text: '10+',
+            color: Colors.red,
+          ), // No badge for "Home"
+          const MotionBadgeWidget(
+            text: '10+',
+            color: Colors.red,
+          ), // No badge for "Profile"
+          const MotionBadgeWidget(
+            text: '10+',
+            color: Colors.red,
+          ), // No badge for "Profile"
+        ],
+        tabSize: 50,
+        tabBarHeight: 55,
+        textStyle: const TextStyle(
+          fontSize: 12,
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+        ),
+        tabIconColor: Colors.grey,
+        tabIconSize: 28.0,
+        tabIconSelectedSize: 26.0,
+        tabSelectedColor: Colors.deepPurple,
+        tabIconSelectedColor: Colors.white,
+        tabBarColor: Colors.white,
+        onTabItemSelected: (int index) {
+          setState(() {
+            _motionTabBarController.index = index;
+          });
+        },
+      ),
+    );
+  }
+
+}
